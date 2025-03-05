1f2b18393100aaf8c4617548a8e041bb745fd3cf
scox1502 <samuelc506@deltalearns.ca>
2025-03-05 17:52:45 +0000

    did the practise

---
+++ practice.py
@@ -0,0 +1,19 @@
+messages = []
+
+while True:
+    message1 = ("hello")
+    messages.append(message1)
+
+    message2 = ("hi")
+    messages.append(message2)
+
+    message3 = ("goodbye")
+    messages.append(message3)
+
+    message4 = ("bye")
+    messages.append(message4)
+    break
+send_message = (messages)
+
+
+print(send_message)
